d2723ee57b22b25b95960309da068d252df8d3f5
Olivia <olivia.gincu@gmail.com>
2025-06-02 16:30:38 +0300

    Add JWT generation with role/permissions claims

---
+++ server/utils/generateToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET = 'super-secret'; 
+
+
+function generateToken(req, res) {
+  const { role = 'VISITOR', permissions = ['READ'] } = req.body;
+
+  const token = jwt.sign({ role, permissions }, SECRET, { expiresIn: '1m' });
+
+  res.json({ token });
+}
+
+
+module.exports = {
+  generateToken,
+  SECRET,
+};
