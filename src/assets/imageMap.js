73023f1f2c599cfe733f33947c9007e9a6d0c762
livia994 <144347901+livia994@users.noreply.github.com>
2026-01-13 13:22:46 +0200

    Create imageMap.js

---
+++ src/assets/imageMap.js
@@ -0,0 +1,36 @@
+import bakery from "./images/bakery.jpg";
+import hero from "./images/hero.jpg";
+import myimage from "./images/myimage.jpg";
+import team1 from "./images/team1.jpg";
+import team2 from "./images/team2.jpg";
+
+import c1 from "./images/c1.jpg";
+import c2 from "./images/c2.jpg";
+import c3 from "./images/c3.jpg";
+import c4 from "./images/c4.jpg";
+import c5 from "./images/c5.jpg";
+import c6 from "./images/c6.jpg";
+import c7 from "./images/c7.jpg";
+import c8 from "./images/c8.jpg";
+import c9 from "./images/c9.jpg";
+import c10 from "./images/c10.jpg";
+
+const imageMap = {
+  bakery,
+  hero,
+  myimage,
+  team1,
+  team2,
+  c1,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  c8,
+  c9,
+  c10,
+};
+
+export default imageMap;
